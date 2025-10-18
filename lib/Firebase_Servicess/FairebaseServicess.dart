37f30df71a1c1f5550a307db1f89a9dafb193877
MARWAN <mh7005600@gmail.com>
2025-10-19 01:47:45 +0300

    finished  the all app

--- lib/Firebase_Servicess/FairebaseServicess.dart
+++ lib/Firebase_Servicess/FairebaseServicess.dart
@@ -21,13 +21,13 @@ class Fairebaeservices {
   }
 
   static addUasertoFireStore(UserModel user) {
-    ///>>>>> this way to create  the collection  to firestore that store the id and the name and email
+    ///>>>>> this normal  way to create  the collection  to firestore that store or reserve the place like: the id and the name and email
     // FirebaseFirestore db = FirebaseFirestore.instance;
     // // CollectionReference <Map<String, dynamic>> usercollection = db.collection(
     // //     "users");
     // // DocumentReference <Map<String, dynamic>> UserDocument = usercollection.doc(
     // //     user.id);
-    ///>>>>> this another way way to create  the collection  to firestore that store the id and the name and email
+    ///>>>>> this another way way to create  the collection(by using with  in  class user model model)  to firestore that store the id and the name and email
     FirebaseFirestore db=FirebaseFirestore.instance;
     CollectionReference<UserModel> usercollection = db.collection("Users").withConverter<UserModel>(
       fromFirestore: (snapshot, _) => UserModel.fromjson(snapshot.data()!),
@@ -53,28 +53,50 @@ class Fairebaeservices {
 // DocumentSnapshot<Map<String,dynamic>> documentSnapshot = await  UserDocument.get();
 // var jason= documentSnapshot.data();
     // return UserModel(name: jason?['name'], email: jason?["email"], id: jason?["id"]);
-///>>>>>>>>> this is another way to get  the user id and details: of the
+    ///>>>>>>>>> this is another way to get  the user id and details: of the
     FirebaseFirestore db=FirebaseFirestore.instance;
-  CollectionReference<UserModel> usercollection = db.collection("Users").withConverter<UserModel>(
-    fromFirestore: (snapshot, _) => UserModel.fromjson(snapshot.data()!),
-    toFirestore: (user, _) => user.toJosn(),
-  );
-  DocumentReference<UserModel> UserDocument = usercollection.doc(id);
-  DocumentSnapshot<UserModel> documentSnapshot = await  UserDocument.get();
-return  documentSnapshot.data();
+    CollectionReference<UserModel> usercollection = db.collection("Users").withConverter<UserModel>(
+      fromFirestore: (snapshot, _) => UserModel.fromjson(snapshot.data()!),
+      toFirestore: (user, _) => user.toJosn(),
+    );
+    DocumentReference<UserModel> UserDocument = usercollection.doc(id);
+    DocumentSnapshot<UserModel> documentSnapshot = await  UserDocument.get();
+    return  documentSnapshot.data();
 
   }
- static Future<void> AddeventtoFirestore(EventModel event,BuildContext context){
+  /// its the same way but he make a function as a short cut
+  static CollectionReference<EventModel> getEventCollection(BuildContext context){
     FirebaseFirestore db=FirebaseFirestore.instance;  /// this mean now we have object of data base
-CollectionReference<EventModel>eventscollection= db.collection("Events").withConverter<EventModel>(
-    fromFirestore: (snapshot,_)=>EventModel.fromjson(snapshot.data()!,context),
-    toFirestore: (event, _ )=>event.toJosn()); /// now we jsoncraete  the collection
-eventscollection.doc();
-DocumentReference<EventModel> eventdocument=eventscollection.doc();
-event.eveintid=eventdocument.id;
-return eventdocument.set(event);
+    CollectionReference<EventModel>eventscollection= db.collection("Events").withConverter<EventModel>(
+        fromFirestore: (snapshot,_)=>EventModel.fromjson(snapshot.data()!,context),
+        toFirestore: (event, _ )=>event.toJosn()); /// now we json craete  the collection  with   rommse or reservied  places
+    return  eventscollection;
+  }
 
+  /// // its the same way to add the  id and title and discription to firestore
+  static Future<void> AddeventtoFirestore(EventModel event,BuildContext context){
+    CollectionReference <EventModel>eventcollection=getEventCollection(context);
+    /// its the  normal way  to  craeate the collection and add the data to fire store
+    ///
+//     FirebaseFirestore db=FirebaseFirestore.instance;  /// this mean now we have object of data base
+// CollectionReference<EventModel>eventscollection= db.collection("Events").withConverter<EventModel>(
+//     fromFirestore: (snapshot,_)=>EventModel.fromjson(snapshot.data()!,context),
+//     toFirestore: (event, _ )=>event.toJosn()); /// now we json craete  the collection  with   rommse or reservied  places
+// eventscollection.doc();
+    DocumentReference<EventModel> eventdocument=eventcollection.doc();
+    event.eveintid=eventdocument.id;
+    return eventdocument.set(event);
+  }
+
+
+
+  /// to return what the events in that last craeated collection
+  static Future<List<EventModel>> getEvents(BuildContext context)async{
+    CollectionReference <EventModel>eventcollection=getEventCollection(context);
+    QuerySnapshot<EventModel> querySnapshot=await eventcollection.get();
+    List<EventModel>events=
+    querySnapshot.docs.map((documnetSnapshot) => documnetSnapshot.data()).toList();
+    return events;
 
   }
 }
-
